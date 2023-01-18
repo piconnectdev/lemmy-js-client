58e602fec5d804bf2ef2d42cd0c80387e655b33d
Dinh Duong Ha <dinhduongha@gmail.com>
2023-01-18 23:31:20 +0700

    Update PiPaymentDTO

--- src/interfaces/api/person.ts
+++ src/interfaces/api/person.ts
@@ -500,6 +500,34 @@ export class PiTipResponse {
   paymentid: string;
 }
 
+export class PiPaymentDTO {
+  // Payment data:
+  identifier: string; // The payment identifier
+  user_uid: string; // The user's app-specific ID
+  amount: number; // The payment amount
+  memo: string; // A string provided by the developer, shown to the user
+  metadata?: object; // An object provided by the developer for their own usage
+  to_address: string; // The recipient address of the blockchain transaction
+  created_at: string; // The payment's creation timestamp
+
+  // Status flags representing the current state of this payment
+  status: {
+    developer_approved: boolean; // Server-Side Approval
+    transaction_verified: boolean; // Blockchain transaction verified
+    developer_completed: boolean; // Server-Side Completion
+    cancelled: boolean; // Cancelled by the developer or by Pi Network
+    user_cancelled: boolean; // Cancelled by the user
+  };
+
+  // Blockchain transaction data:
+  transaction: null | {
+    // This is null if no transaction has been made yet
+    txid: string; // The id of the blockchain transaction
+    verified: boolean; // True if the transaction matches the payment, false otherwise
+    _link: string; // A link to the operation on the Blockchain API
+  };
+}
+
 export class PiPaymentFound {
   pi_username: string;
   pi_token: string;
@@ -515,6 +543,7 @@ export class PiPaymentFound {
   comment?: string;
 
   auth?: string;
+  dto?: PiPaymentDTO;
 }
 
 export class PiPaymentFoundResponse {
